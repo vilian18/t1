2ca9fb050a32716658e9a00648a5e56ee528af11
vilian18 <vpavanatto@inf.ufsm.br>
2015-03-12 23:40:43 -0300

    parte que testa e recebe o argumento pronta

--- LABII100315.c
+++ LABII100315.c
@@ -12,11 +12,16 @@ void le_alunos(int* matriculas, char** nomes, int* n){
    int mat, i=0, linha=0;
    char nome[50]={0};
    char c;
-   FILE *f = fopen("alunos.txt", "R");
+   FILE *f;
+   f = fopen("aluno.txt", "r");
+   if(f==NULL){
+      printf("Erro ao abrir arquivo.\n");
+      exit(1);
+      }
    while(feof(f)!=0){
-      if(fscanf(F, "%d, &mat")==0)
+      if(fscanf(f, "%d, &mat")==0)
          break;
-      c=fgetc(F);
+      c=fgetc(f);
       while(c != '\n'){
          nome[i]=c;
          c=fgetc(f);
@@ -33,6 +38,15 @@ void le_alunos(int* matriculas, char** nomes, int* n){
 int main (int argc, char **argv){
    char nomes[50][50]={0};
    int matricula[50];
+   char* nome_argumento= NULL;
+   if(argc>1){
+      nome_argumento= argv[1];
+      printf("%s\n", nome_argumento);
+   }
+   else{
+      printf("Ha argumento faltando para o programa!\n");
+      exit(0);
+   }
 
    return 0;
 }
@@ -47,3 +61,4 @@ fgetc --> le 1 caracter
 feof --> testar se chegou no final do arquivo
 if(feof(F)==0) --> significa o final do arquivo
    fclose(F);
+*/
